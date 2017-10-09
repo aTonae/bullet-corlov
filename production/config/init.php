3a60be57190b7ca9c79eb12e8e10a8eacba1b3ad
ato <kmagrawal.13@gmail.com>
2017-10-09 13:05:55 +0530

    config update

--- production/config/init.php
+++ production/config/init.php
@@ -1,25 +1,10 @@
 <?php
-$db_name = "cptas_array";
-$db_user = "obl_array";
-$db_pass = "obl@array";
-$server = "obleamlabs.com";
-$con = mysqli_connect($server , $db_user ,$db_pass , $db_name);
-if(!$con)
-{
-// echo "connection error".mysqli_connect_error();
-}
-else
-{
-// echo "<h3> Database Connection Success</ h3>";
-}
+
 
 class DbInit{
 
 
-	public $db_name = "cptas_array";
-	public $db_user = "obl_array";
-	public $db_pass = "obl@array";
-	public $server = "obleamlabs.com";
+	
 
 	public $con;
 
